9b97f111e7fe90090cb551edd4d14a486c57da14
Tuan Dang <tuan3dang@gmail.com>
2024-02-10 15:06:08 -0500

    Redirect from start

--- src/components/OptionMenu/Options.tsx
+++ src/components/OptionMenu/Options.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import FadeIn from "../Animation/FadeIn";
 
 import {
   Form,
