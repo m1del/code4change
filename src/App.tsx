f009034ebafa575a85406e9246a3dc517f9148f8
John Miclat <miclatjohn1@gmail.com>
2024-02-10 11:41:49 -0500

    fixes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="underline">Hello world!</div>
+      <Button>click me!</Button>
+    </>
+  );
+}
