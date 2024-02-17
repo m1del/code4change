b013e776f221d95dad5e72306a8b5416164735c7
John Miclat <miclatjohn1@gmail.com>
2024-02-16 22:39:06 -0500

    init struct

---
+++ src2/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import Start from "./components/StartMenu/Start";
+import Options from "./components/OptionMenu/Options";
+import Workout from "./components/Workout/Workout";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="text-secondary flex items-center justify-center pb-36 bg-primary">
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/options" element={<Options />} />
+          <Route path="/workout" element={<Workout />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
